2f424b77a64de73b272d2d1af1081a35c232ae47
Richard Macias <rjny86@gmail.com>
2016-10-08 18:42:37 -0400

    fem-3

---
+++ js/ClientApp.js
@@ -0,0 +1,24 @@
+var div = React.DOM.div
+var h1  = React.DOM.h1
+
+var MyTitle = React.createClass({
+  render(){
+    return (
+      div(null,
+        h1({style: {color: this.props.color}}, this.props.title)
+      )
+    )
+  }
+})
+
+var MyTitleFact = React.createFactory(MyTitle)
+
+var MyFirstComponent = (
+  div(null,
+    MyTitleFact({title: 'props are great!', color: 'rebeccapurple'}),
+    MyTitleFact({title: 'use props everywhere!', color: 'mediumaquamarine'}),
+    MyTitleFact({title: 'Props are the best!', color: 'peru'})
+  )
+)
+
+ReactDOM.render(MyFirstComponent, document.getElementById('app'))
